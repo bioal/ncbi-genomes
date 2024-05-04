0da38de20fce9f28c2ce53db1414ed18a4ac88a8
Hirokazu Chiba <hchiba1@gmail.com>
2024-05-04 21:08:27 +0900

    Modify bin/extract_eukaryotes.pl to sort output

--- bin/extract_eukaryotes.pl
+++ bin/extract_eukaryotes.pl
@@ -10,19 +10,43 @@ my $USAGE=
 my %OPT;
 getopts('', \%OPT);
 
+my %EUKARYOTES = (
+    "vertebrate_mammalian" => 1,
+    "vertebrate_other" => 1,
+    "invertebrate" => 1,
+    "plant" => 1,
+    "fungi" => 1,
+    "protozoa" => 1,
+    );
+
+my %OTHERS = (
+    "bacteria" => 1,
+    "archaea" => 1,
+    "viral" => 1,
+    );
+
+my %GENOME;
 while (<>) {
     chomp;
     if (/^#/) {
         next;
     }
+
     my @f = split(/\t/, $_, -1);
     if (@f != 38) {
         die $_;
     }
-    if ($f[24] =~ /vertebrate|plant|fungi|protozoa/) {
-        print "$_\n";
-    } elsif ($f[24] =~ /bacteria|archaea|viral/) {
+    my $id = $f[0];
+    my $group = $f[24];
+
+    if ($EUKARYOTES{$group}) {
+        $GENOME{$id} = $_;
+    } elsif ($OTHERS{$group}) {
     } else {
         die $_;
     }
+}
+
+for my $id (sort keys %GENOME) {
+    print "$GENOME{$id}\n";
 }
