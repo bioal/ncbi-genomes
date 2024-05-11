da43af144b843e7df5fc77e495a0ae049f318e8c
Hirokazu Chiba <hchiba1@gmail.com>
2024-05-11 10:49:52 +0900

    Add bin/extract_groups.pl -a option to output all

--- bin/extract_groups.pl
+++ bin/extract_groups.pl
@@ -5,28 +5,33 @@ use Getopt::Std;
 my $PROGRAM = basename $0;
 my $USAGE=
 "Usage: $PROGRAM assembly_summary_refseq.txt > group_count.txt
+-a: output all groups
 ";
 
 my %OPT;
-getopts('', \%OPT);
+getopts('a', \%OPT);
 
 !@ARGV && -t and die $USAGE;
 
 ### Predefined groups
-my @GROUPS = (
+my @EUKARYOTES = (
     "vertebrate_mammalian",
     "vertebrate_other",
     "invertebrate",
     "plant",
     "fungi",
     "protozoa",
+    );
+my @PROKARYOTES = (
     "bacteria",
     "archaea",
+    );
+my @VIRUSES = (
     "viral",
-    );    
+    );
 
 my %GROUP;
-for my $group (@GROUPS) {
+for my $group (@EUKARYOTES, @PROKARYOTES, @VIRUSES) {
     $GROUP{$group} = 1;
 }
 
@@ -52,10 +57,38 @@ while (<>) {
     }
 }
 
-### Print counts for each group in the order of @GROUPS
-for my $group (@GROUPS) {
+### Print counts for each group in the order of predefined groups
+my $out_eukaryotes = "";
+my $count_eukaryotes = 0;
+for my $group (@EUKARYOTES) {
     my $count = $COUNT{$group} || 0;
-    print "$count\t$group\n";
+    $count_eukaryotes += $count;
+    $out_eukaryotes .= "$count\t$group\n";
+}
+if ($OPT{a} || $count_eukaryotes) {
+    print $out_eukaryotes;
+}
+
+my $out_prokaryotes = "";
+my $count_prokaryotes = 0;
+for my $group (@PROKARYOTES) {
+    my $count = $COUNT{$group} || 0;
+    $count_prokaryotes += $count;
+    $out_prokaryotes .= "$count\t$group\n";
+}
+if ($OPT{a} || $count_prokaryotes) {
+    print $out_prokaryotes;
+}
+
+my $out_viruses = "";
+my $count_viruses = 0;
+for my $group (@VIRUSES) {
+    my $count = $COUNT{$group} || 0;
+    $count_viruses += $count;
+    $out_viruses .= "$count\t$group\n";
+}
+if ($OPT{a} || $count_viruses) {
+    print $out_viruses;
 }
 
 ### Print other categories if any detected
