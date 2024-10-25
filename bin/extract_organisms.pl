e9a19684db0bdece94a6c21fce55be632c0fa957
Hirokazu Chiba <hchiba1@gmail.com>
2024-10-25 15:27:18 +0900

    Update extract_organisms.pl for sort and default input

--- bin/extract_organisms.pl
+++ bin/extract_organisms.pl
@@ -5,10 +5,11 @@ use Getopt::Std;
 my $PROGRAM = basename $0;
 my $USAGE=
 "Usage: $PROGRAM TAXON...
+-s: sort by ID
 ";
 
 my %OPT;
-getopts('', \%OPT);
+getopts('s', \%OPT);
 
 if (!@ARGV) {
     print STDERR $USAGE;
@@ -26,23 +27,50 @@ for my $arg (@ARGV) {
     }
 }
 
-my $TAXID_COL = 0; # default column for taxid
-while (<STDIN>) {
-    chomp;
-    if (/^#+  /) {
-        next;
-    }
-    my @f = split(/\t/, $_, -1);
-    if (/^#/) {
-        print "$_\n";
-        for (my $i=0; $i<@f; $i++) {
-            if ($f[$i] eq "taxid") {
-                $TAXID_COL = $i;
-                last;
+if (-t) {
+    open(my $fh, "/home/chiba/github/bioal/ncbi-genomes/data/assembly_summary_refseq.txt") || die "$!";
+    extract_data($fh);
+    close($fh);
+} else {
+    extract_data(*STDIN);
+}
+
+################################################################################
+### Function ###################################################################
+################################################################################
+
+sub extract_data {
+    my ($fh) = @_;
+
+    my %GENOME;
+    my $TAXID_COL = 0; # default column for taxid
+    while (<$fh>) {
+        chomp;
+        if (/^#+  /) {
+            next;
+        }
+        my @f = split(/\t/, $_, -1);
+        my $id = $f[0];
+        if (/^#/) {
+            print "$_\n";
+            for (my $i=0; $i<@f; $i++) {
+                if ($f[$i] eq "taxid") {
+                    $TAXID_COL = $i;
+                    last;
+                }
+            }
+        }
+        if ($TAXON{$f[$TAXID_COL]}) {
+            if ($OPT{s}) {
+                $GENOME{$id} = $_;
+            } else {
+                print "$_\n";
             }
         }
     }
-    if ($TAXON{$f[$TAXID_COL]}) {
-        print "$_\n";
+    if ($OPT{s}) {
+        for my $id (sort keys %GENOME) {
+            print "$GENOME{$id}\n";
+        }
     }
 }
