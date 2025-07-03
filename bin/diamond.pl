c989771a41ed94c5a27058c6532633c4f7fd72d0
Hirokazu Chiba <hchiba1@gmail.com>
2025-07-03 17:28:02 +0900

    Modify diamond.pl for log_file

--- bin/diamond.pl
+++ bin/diamond.pl
@@ -21,7 +21,7 @@ if (@ARGV != 2) {
 my ($query, $target) = @ARGV;
 
 my $out_file = "$query-$target.tsv";
-my $err_file = "$query-$target.err";
+my $log_file = "$query-$target.log";
 
 my $options = "";
 if ($OPT{f}) {
@@ -34,9 +34,9 @@ if ($OPT{f}) {
 if ($OPT{t}) {
     my $outfmt = "";
     $outfmt .= "qtitle stitle";
-    system "diamond blastp $options -k 1000 -q $query -d $target --outfmt 6 qseqid sseqid bitscore $outfmt -o $out_file 2> $err_file";
+    system "diamond blastp $options -k 1000 -q $query -d $target --outfmt 6 qseqid sseqid bitscore $outfmt -o $out_file 2> $log_file";
 } else {
-    system "diamond blastp $options -k 1000 -q $query -d $target -o $out_file 2> $err_file";
+    system "diamond blastp $options -k 1000 -q $query -d $target -o $out_file 2> $log_file";
 }
 
 if ($OPT{s}) {
