de94e1584ba358c73a0108a6096ec168f934d390
Hirokazu Chiba <hchiba1@gmail.com>
2026-03-23 17:56:06 +0900

    Update bin/diamond.pl for input file names

--- bin/diamond.pl
+++ bin/diamond.pl
@@ -18,10 +18,12 @@ if (@ARGV != 2) {
     print STDERR $USAGE;
     exit 1;
 }
-my ($query, $target) = @ARGV;
+my ($file1, $file2) = @ARGV;
+my $name1 = basename $file1;
+my $name2 = basename $file2;
 
-my $out_file = "$query-$target.tsv";
-my $log_file = "$query-$target.log";
+my $out_file = "$name1-$name2.tsv";
+my $log_file = "$name1-$name2.log";
 
 my $options = "";
 if ($OPT{f}) {
@@ -34,9 +36,9 @@ if ($OPT{f}) {
 if ($OPT{t}) {
     my $outfmt = "";
     $outfmt .= "qtitle stitle";
-    system "diamond blastp $options -k 1000 -q $query -d $target --outfmt 6 qseqid sseqid bitscore $outfmt -o $out_file 2> $log_file";
+    system "diamond blastp $options -k 1000 -q $file1 -d $file2 --outfmt 6 qseqid sseqid bitscore $outfmt -o $out_file 2> $log_file";
 } else {
-    system "diamond blastp $options -k 1000 -q $query -d $target -o $out_file 2> $log_file";
+    system "diamond blastp $options -k 1000 -q $file1 -d $file2 -o $out_file 2> $log_file";
 }
 
 if ($OPT{s}) {
