2977d2225ce82d7865ddc954c8029de317ca2fc6
Hirokazu Chiba <hchiba1@gmail.com>
2026-04-02 19:28:02 +0900

    Modify bin/select_paralog.pl to output paralogs

--- bin/select_paralog.pl
+++ bin/select_paralog.pl
@@ -32,6 +32,7 @@ close(CROSS_SPECIES_SCORE);
 
 open(INTRA_SPECIES, "$INTRA_SPECIES") || die "$!";
 my %THRESHOLD;
+my %PARALOG;
 while (<INTRA_SPECIES>) {
     chomp;
     my @f = split(/\t/, $_, -1);
@@ -50,9 +51,28 @@ while (<INTRA_SPECIES>) {
         $THRESHOLD{$gene1} > $score) {
         next;
     }
-    print $_,  "\t", $paralogy, "\n";
+    if (! defined $PARALOG{$gene1}) {
+        $PARALOG{$gene1} = "";
+    }
+    if ($paralogy >= 1) {
+        if ($PARALOG{$gene1}) {
+            $PARALOG{$gene1} .= ",$gene2";
+        } else {
+            $PARALOG{$gene1} = $gene2;
+        }
+    }
     if ($paralogy < 1) {
         $THRESHOLD{$gene1} = $score;
     }
+    print $_,  "\t", $paralogy;
+
+    print "\t";
+    if ($paralogy < 1) {
+        print $PARALOG{$gene1};
+    } else {
+        print "";
+    }
+
+    print "\n";
 }
 close(INTRA_SPECIES);
