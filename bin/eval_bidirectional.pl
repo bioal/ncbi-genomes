a77845797db090acc121ab5e3cc9c8160ef67cbd
Hirokazu Chiba <hchiba1@gmail.com>
2026-04-02 20:38:51 +0900

    Refactor bin/eval_bidirectional.pl

--- bin/eval_bidirectional.pl
+++ bin/eval_bidirectional.pl
@@ -35,9 +35,9 @@ while (<HUMAN_MOUSE>) {
 }
 close(HUMAN_MOUSE);
 
-my %MOUSE_HUMAN_BIT_SCORE;
-my %MOUSE_HUMAN_ORTHOLOGY_SCORE;
-my %MOUSE_HUMAN_GROUPED_ORTHOLOGY_SCORE;
+my %REVERSE_BIT_SCORE;
+my %REVERSE_ORTHOLOGY_SCORE;
+my %REVERSE_GROUPED_ORTHOLOGY_SCORE;
 my %MIN_ORTHOLOGY_SCORE;
 open(MOUSE_HUMAN, "$MOUSE_HUMAN") || die "$!";
 while (<MOUSE_HUMAN>) {
@@ -48,9 +48,9 @@ while (<MOUSE_HUMAN>) {
     my $mouse_human_bit_score = $f[11];
     my $mouse_human_orthology_score = $f[12];
     my $mouse_human_grouped_orthology_score = $f[13];
-    $MOUSE_HUMAN_BIT_SCORE{"${human_gene}\t${mouse_gene}"} = $mouse_human_bit_score;
-    $MOUSE_HUMAN_ORTHOLOGY_SCORE{"${human_gene}\t${mouse_gene}"} = $mouse_human_orthology_score;
-    $MOUSE_HUMAN_GROUPED_ORTHOLOGY_SCORE{"${human_gene}\t${mouse_gene}"} = $mouse_human_grouped_orthology_score;
+    $REVERSE_BIT_SCORE{"${human_gene}\t${mouse_gene}"} = $mouse_human_bit_score;
+    $REVERSE_ORTHOLOGY_SCORE{"${human_gene}\t${mouse_gene}"} = $mouse_human_orthology_score;
+    $REVERSE_GROUPED_ORTHOLOGY_SCORE{"${human_gene}\t${mouse_gene}"} = $mouse_human_grouped_orthology_score;
     if ($ORTHOLOGY_SCORE{"${human_gene}\t${mouse_gene}"}) {
         my $human_mouse_orthology_score = $ORTHOLOGY_SCORE{"${human_gene}\t${mouse_gene}"};
         $MIN_ORTHOLOGY_SCORE{"${human_gene}\t${mouse_gene}"} = min($human_mouse_orthology_score, $mouse_human_orthology_score);
@@ -73,8 +73,8 @@ while (<BIT_SCORES>) {
     }
     my $human_mouse = "${human_gene}\t${mouse_gene}";
 
-    if ($MOUSE_HUMAN_BIT_SCORE{$human_mouse} &&
-        $MOUSE_HUMAN_ORTHOLOGY_SCORE{$human_mouse} <= 1 &&
+    if ($REVERSE_BIT_SCORE{$human_mouse} &&
+        $REVERSE_ORTHOLOGY_SCORE{$human_mouse} <= 1 &&
         $BIT_SCORE{$human_mouse} &&
         $ORTHOLOGY_SCORE{$human_mouse} <= 1 ) {
         # print_result($human_gene, $mouse_gene);
@@ -98,19 +98,19 @@ sub insufficient_orthology_score {
     my ($human_mouse) = @_;
 
     if (! $BIT_SCORE{$human_mouse} &&
-          $MOUSE_HUMAN_ORTHOLOGY_SCORE{$human_mouse} <= 1) {
+          $REVERSE_ORTHOLOGY_SCORE{$human_mouse} <= 1) {
         return 1;
     }
 
-    if (! $MOUSE_HUMAN_BIT_SCORE{$human_mouse} &&
+    if (! $REVERSE_BIT_SCORE{$human_mouse} &&
           $ORTHOLOGY_SCORE{$human_mouse} <= 1 ) {
         return 1;
     }
 
     if ($ORTHOLOGY_SCORE{$human_mouse}   && $ORTHOLOGY_SCORE{$human_mouse}   <= 1 &&
-        $MOUSE_HUMAN_ORTHOLOGY_SCORE{$human_mouse}   && $MOUSE_HUMAN_ORTHOLOGY_SCORE{$human_mouse}   <= 1 &&
+        $REVERSE_ORTHOLOGY_SCORE{$human_mouse}   && $REVERSE_ORTHOLOGY_SCORE{$human_mouse}   <= 1 &&
         $GROUPED_ORTHOLOGY_SCORE{$human_mouse} && $GROUPED_ORTHOLOGY_SCORE{$human_mouse} <= 0.9 &&
-        $MOUSE_HUMAN_GROUPED_ORTHOLOGY_SCORE{$human_mouse} && $MOUSE_HUMAN_GROUPED_ORTHOLOGY_SCORE{$human_mouse} <= 0.9
+        $REVERSE_GROUPED_ORTHOLOGY_SCORE{$human_mouse} && $REVERSE_GROUPED_ORTHOLOGY_SCORE{$human_mouse} <= 0.9
         ) {
         return 1;
     }
@@ -124,13 +124,13 @@ sub print_result {
                $human_gene,
                $mouse_gene,
                $BIT_SCORE{$human_mouse} || 0,
-               $MOUSE_HUMAN_BIT_SCORE{$human_mouse} || 0,
+               $REVERSE_BIT_SCORE{$human_mouse} || 0,
                $ORTHOLOGY_SCORE{$human_mouse} || 0,
-               $MOUSE_HUMAN_ORTHOLOGY_SCORE{$human_mouse} || 0,
+               $REVERSE_ORTHOLOGY_SCORE{$human_mouse} || 0,
                $MIN_ORTHOLOGY_SCORE{$human_mouse} || 0,
                $GROUPED_ORTHOLOGY_SCORE{$human_mouse} || 0,
-               $MOUSE_HUMAN_GROUPED_ORTHOLOGY_SCORE{$human_mouse} || 0,
-               min($GROUPED_ORTHOLOGY_SCORE{$human_mouse} || 0, $MOUSE_HUMAN_GROUPED_ORTHOLOGY_SCORE{$human_mouse} || 0)
+               $REVERSE_GROUPED_ORTHOLOGY_SCORE{$human_mouse} || 0,
+               min($GROUPED_ORTHOLOGY_SCORE{$human_mouse} || 0, $REVERSE_GROUPED_ORTHOLOGY_SCORE{$human_mouse} || 0)
         ), "\n";
 }
 
