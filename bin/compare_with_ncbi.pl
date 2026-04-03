57674dcd898439ae89ebed9c1721d2e980e12666
Hirokazu Chiba <hchiba1@gmail.com>
2026-04-03 19:24:01 +0900

    Simplify bin/compare_with_ncbi.pl

--- bin/compare_with_ncbi.pl
+++ bin/compare_with_ncbi.pl
@@ -10,16 +10,10 @@ my $USAGE=
 my %OPT;
 getopts('', \%OPT);
 
-my @FILES = (
-    "/home/chiba/github/hchiba1/human-mouse/ncbi_orthologs/human-mouse.2026-04-02"
-    );
-my $N_FILES = @FILES;
-
 my %HASH;
-for (my $i = 0; $i < $N_FILES; $i++) {
-    my $file = $FILES[$i];
-    read_file($file, $i + 1, \%HASH);
-}
+read_file(
+    "/home/chiba/github/hchiba1/human-mouse/ncbi_orthologs/human-mouse.2026-04-02",
+    \%HASH);
 
 while (<STDIN>) {
     chomp;
@@ -27,10 +21,8 @@ while (<STDIN>) {
     my $gene1 = $f[0];
     my $gene2 = $f[1];
     print $_;
-    for (my $idx = 1; $idx <= $N_FILES; $idx ++) {
-        my $comparison = eval_results($HASH{$idx}, $gene1, $gene2);
-        print "\t$comparison";
-    }
+    my $comparison = eval_results(\%HASH, $gene1, $gene2);
+    print "\t$comparison";
     print "\n";
 }
 
@@ -52,18 +44,8 @@ sub eval_results {
     return "false";
 }
 
-sub match_results {
-    my ($r_hash, $gene1, $gene2) = @_;
-
-    if (${$r_hash}{"${gene1}\t${gene2}"}) {
-        return "true";
-    } else {
-        return "false";
-    }
-}
-
 sub read_file {
-    my ($file, $index, $r_hash) = @_;
+    my ($file, $r_hash) = @_;
 
     open(FILE, "$file") || die "$!";
     while (<FILE>) {
@@ -71,7 +53,7 @@ sub read_file {
         my @f = split(/\t/, $_, -1);
         my $gene1 = $f[0];
         my $gene2 = $f[1];
-        ${$r_hash}{$index}{"${gene1}\t${gene2}"} = 1;
+        ${$r_hash}{"${gene1}\t${gene2}"} = 1;
     }
     close(FILE);
 }
