382b0e2f86e8d3592591c49dea01a569898a70eb
Hirokazu Chiba <hchiba1@gmail.com>
2026-04-03 17:15:04 +0900

    Modify eval_bidirectional.pl to output paralogs

--- bin/eval_bidirectional.pl
+++ bin/eval_bidirectional.pl
@@ -19,6 +19,7 @@ my ($HUMAN_MOUSE, $MOUSE_HUMAN, $BIT_SCORES) = @ARGV;
 
 my %BIT_SCORE;
 my %ORTHOLOGY;
+my %PARALOGS;
 my %GROUPED_ORTHOLOGY;
 open(HUMAN_MOUSE, "$HUMAN_MOUSE") || die "$!";
 while (<HUMAN_MOUSE>) {
@@ -29,14 +30,17 @@ while (<HUMAN_MOUSE>) {
     my $bit_score = $f[11];
     my $orthology = $f[12];
     my $grouped_orthology = $f[13];
+    my $paralogs = $f[14];
     $BIT_SCORE{"${human_gene}\t${mouse_gene}"} = $bit_score;
     $ORTHOLOGY{"${human_gene}\t${mouse_gene}"} = $orthology;
     $GROUPED_ORTHOLOGY{"${human_gene}\t${mouse_gene}"} = $grouped_orthology;
+    $PARALOGS{"${human_gene}\t${mouse_gene}"} = $paralogs;
 }
 close(HUMAN_MOUSE);
 
 my %REVERSE_BIT_SCORE;
 my %REVERSE_ORTHOLOGY;
+my %REVERSE_PARALOGS;
 my %REVERSE_GROUPED_ORTHOLOGY;
 my %MIN_ORTHOLOGY;
 open(MOUSE_HUMAN, "$MOUSE_HUMAN") || die "$!";
@@ -48,9 +52,11 @@ while (<MOUSE_HUMAN>) {
     my $bit_score = $f[11];
     my $orthology = $f[12];
     my $grouped_orthology = $f[13];
+    my $paralogs = $f[14];
     $REVERSE_BIT_SCORE{"${human_gene}\t${mouse_gene}"} = $bit_score;
     $REVERSE_ORTHOLOGY{"${human_gene}\t${mouse_gene}"} = $orthology;
     $REVERSE_GROUPED_ORTHOLOGY{"${human_gene}\t${mouse_gene}"} = $grouped_orthology;
+    $REVERSE_PARALOGS{"${human_gene}\t${mouse_gene}"} = $paralogs;
     if ($ORTHOLOGY{"${human_gene}\t${mouse_gene}"}) {
         my $human_mouse_orthology = $ORTHOLOGY{"${human_gene}\t${mouse_gene}"};
         $MIN_ORTHOLOGY{"${human_gene}\t${mouse_gene}"} = min($human_mouse_orthology, $orthology);
@@ -120,6 +126,12 @@ sub insufficient_orthology {
 sub print_result {
     my ($human_gene, $mouse_gene) = @_;
     my $human_mouse = "${human_gene}\t${mouse_gene}";
+    if ($PARALOGS{$human_mouse}) {
+        $human_gene .= "," . $PARALOGS{$human_mouse};
+    }
+    if ($REVERSE_PARALOGS{$human_mouse}) {
+        $mouse_gene .= "," . $REVERSE_PARALOGS{$human_mouse};
+    }
     print join("\t",
                $human_gene,
                $mouse_gene,
