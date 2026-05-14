edeabef42fbe33934956db3d163b3d8254efd961
Hirokazu Chiba <hchiba1@gmail.com>
2026-05-14 23:46:02 +0900

    Modify get_syntenic_block.pl: input from stdin

--- bin/get_syntenic_block.pl
+++ bin/get_syntenic_block.pl
@@ -45,28 +45,6 @@ read_gene_info(
 my %ORTHOLOG;
 read_orthology("/home/chiba/github/bioal/human-mouse/human-mouse.v1.tsv");
 
-if (!@ARGV) {
-    print STDERR $USAGE;
-    exit 1;
-}
-my @GENE = split(/,/, join(",", @ARGV));
-
-my %GENE;
-my @INPUT_GENE;
-for my $gene (@GENE) {
-    if ($gene =~ /^\d+$/) {
-        $GENE{$gene} = 1;
-        push @INPUT_GENE, $gene;
-    } elsif ($SYMBOL2ID{$gene}) {
-        for my $geneid (@{$SYMBOL2ID{$gene}}) {
-            $GENE{$geneid} = 1;
-            push @INPUT_GENE, $geneid;
-        }
-    } else {
-        print STDERR "No such symbol: $gene\n";
-    }
-}
-
 my @NC;
 my %FOUND;
 my %REVERSE_ORIENTATION;
@@ -76,106 +54,153 @@ for (my $i=0; $i<@LINE; $i++) {
     my $gene = $f[1];
     my $orientation = $f[4];
     $NC[$i] = $nc;
-    if ($GENE{$gene}) {
-        $FOUND{$gene} = $i;
-    }
+    $FOUND{$gene} = $i;
     if ($orientation eq "-") {
         $REVERSE_ORIENTATION{$gene} = 1;
     }
 }
 
-# Get lines of each syntenic block
-my %FOUND_LINES;
-my %REMEMBER_GENE;
-my %SYMBOL_MAX_LEN;
-my $COUNT_GENES = 0;
-for my $anchor (keys %FOUND) {
-    my $i = $FOUND{$anchor};
-    my $start_idx = get_start_idx($i);
-    my $end_idx = get_end_idx($i);
-    my @line;
-    for (my $j=$start_idx; $j<=$end_idx; $j++) {
-        my @f = split(/\t/, $LINE[$j], -1);
-        my $gene = $f[1];
-        $REMEMBER_GENE{$gene} = 1;
-        remember_symbol_max_len($anchor, $gene);
-        push @line, $LINE[$j];
-    }
-    $FOUND_LINES{$anchor} = \@line;
-    $COUNT_GENES += scalar(@line) - 1; # exclude the anchor gene
+!@ARGV && -t and die $USAGE;
+
+if (@ARGV) {
+    my @GENE = split(/,/, join(",", @ARGV));
+    my $ret =get_syntenic_block(@GENE);
+    print "$ret\n";
+    exit;
 }
 
-# Flag genes that have orthologs
-my %HIGHLIGHT_GENE;
-my $COUNT_ORTHOLOGS = 0;
-for my $gene (keys %REMEMBER_GENE) {
-    if ($ORTHOLOG{$gene}) {
-        my $mouse_gene = $ORTHOLOG{$gene};
-        if ($REMEMBER_GENE{$mouse_gene}) {
-            $HIGHLIGHT_GENE{$gene} = 1;
-            $HIGHLIGHT_GENE{$mouse_gene} = 1;
-            $COUNT_ORTHOLOGS += 2;
-        }
-    }
-}
-for my $anchor (keys %FOUND) {
-    my $i = $FOUND{$anchor};
-    my @f = split(/\t/, $LINE[$i], -1);
-    my $gene = $f[1];
-    if ($HIGHLIGHT_GENE{$gene}) {
-        $COUNT_ORTHOLOGS -= 1; # Exclude the anchor gene
-    }
-}
-
-# Format genes in each block
-my %BLOCK;
-for my $anchor (keys %FOUND_LINES) {
-    my $block = "";
-    for my $line (@{$FOUND_LINES{$anchor}}) {
-        my @f = split(/\t/, $line, -1);
-        my $nc = $f[0];
-        my $gene = $f[1];
-        my $start_pos = $f[2];
-        my $end_pos = $f[3];
-        my $orientation = $f[4];
-        $block .= join("\t",
-                     $nc,
-                     $gene,
-                     format_pos($start_pos),
-                     format_pos($end_pos),
-                     $orientation,
-                     format_symbol($anchor, $gene),
-            ) . "\n";
-    }
-    $BLOCK{$anchor} = $block;
-}
-
-# Reverse the gene order if necessary
-my @OUT;
-for my $gene (@INPUT_GENE) {
-    if ($FOUND{$gene}) {
-        my $out = $BLOCK{$gene};
-        if ($REVERSE_ORIENTATION{$gene} && !$OPT{R}) {
-            $out = reverse_block($BLOCK{$gene});
-        }
-        push @OUT, $out;
-    }
-}
-
-if ($OPT{s}) {
-    my $RATIO = $COUNT_ORTHOLOGS / $COUNT_GENES;
-    if ($OPT{v}) {
-        printf("%.1f = $COUNT_ORTHOLOGS / $COUNT_GENES\n", $RATIO * 100);
-    } else {
-        print join("\t", @GENE, $RATIO), "\n";
-    }
-} else {
-    print paste_blocks(@OUT), "\n";
+while (<STDIN>) {
+    chomp;
+    my @f = split(/\t/, $_, -1);
+    my @gene = @f[0,1];
+    my $ret =get_syntenic_block(@gene);
+    print $_, "\t$ret\n";
 }
 
 ################################################################################
 ### Function ###################################################################
 ################################################################################
+
+sub get_syntenic_block {
+    my @GENE = @_;
+
+    my %GENE;
+    my @INPUT_GENE;
+    for my $gene (@GENE) {
+        if ($gene =~ /^\d+$/) {
+            $GENE{$gene} = 1;
+            push @INPUT_GENE, $gene;
+        } elsif ($SYMBOL2ID{$gene}) {
+            for my $geneid (@{$SYMBOL2ID{$gene}}) {
+                $GENE{$geneid} = 1;
+                push @INPUT_GENE, $geneid;
+            }
+        } else {
+            print STDERR "No such symbol: $gene\n";
+        }
+    }
+
+    # Get lines of each syntenic block
+    my %FOUND_LINES;
+    my %REMEMBER_GENE;
+    my %SYMBOL_MAX_LEN;
+    my $COUNT_GENES = 0;
+    for my $anchor (keys %GENE) {
+        if (! defined $FOUND{$anchor}) {
+            print STDERR "not found pos for $anchor\n";
+            next;
+        }
+        my $i = $FOUND{$anchor};
+        my $start_idx = get_start_idx($i);
+        my $end_idx = get_end_idx($i);
+        my @line;
+        for (my $j=$start_idx; $j<=$end_idx; $j++) {
+            my @f = split(/\t/, $LINE[$j], -1);
+            my $gene = $f[1];
+            $REMEMBER_GENE{$gene} = 1;
+            remember_symbol_max_len(\%SYMBOL_MAX_LEN, $gene, $anchor);
+            push @line, $LINE[$j];
+        }
+        $FOUND_LINES{$anchor} = \@line;
+        $COUNT_GENES += scalar(@line) - 1; # exclude the anchor gene
+    }
+
+    # Flag genes that have orthologs
+    my %HIGHLIGHT_GENE;
+    my $COUNT_ORTHOLOGS = 0;
+    for my $gene (keys %REMEMBER_GENE) {
+        if ($ORTHOLOG{$gene}) {
+            my $mouse_gene = $ORTHOLOG{$gene};
+            if ($REMEMBER_GENE{$mouse_gene}) {
+                $HIGHLIGHT_GENE{$gene} = 1;
+                $HIGHLIGHT_GENE{$mouse_gene} = 1;
+                $COUNT_ORTHOLOGS += 2;
+            }
+        }
+    }
+    for my $anchor (keys %GENE) {
+        if (! defined $FOUND{$anchor}) {
+            print STDERR "not found pos for $anchor\n";
+            next;
+        }
+        my $i = $FOUND{$anchor};
+        my @f = split(/\t/, $LINE[$i], -1);
+        my $gene = $f[1];
+        if ($HIGHLIGHT_GENE{$gene}) {
+            $COUNT_ORTHOLOGS -= 1; # Exclude the anchor gene
+        }
+    }
+
+    # Format genes in each block
+    my %BLOCK;
+    for my $anchor (keys %FOUND_LINES) {
+        my $block = "";
+        for my $line (@{$FOUND_LINES{$anchor}}) {
+            my @f = split(/\t/, $line, -1);
+            my $nc = $f[0];
+            my $gene = $f[1];
+            my $start_pos = $f[2];
+            my $end_pos = $f[3];
+            my $orientation = $f[4];
+            $block .= join("\t",
+                           $nc,
+                           $gene,
+                           format_pos($start_pos),
+                           format_pos($end_pos),
+                           $orientation,
+                           format_symbol($gene, \%GENE, \%HIGHLIGHT_GENE, $SYMBOL_MAX_LEN{$anchor}),
+                ) . "\n";
+        }
+        $BLOCK{$anchor} = $block;
+    }
+
+    # Reverse the gene order if necessary
+    my @OUT;
+    for my $gene (@INPUT_GENE) {
+        if ($FOUND{$gene}) {
+            my $out = $BLOCK{$gene};
+            if ($REVERSE_ORIENTATION{$gene} && !$OPT{R}) {
+                $out = reverse_block($BLOCK{$gene});
+            }
+            push @OUT, $out;
+        }
+    }
+
+    my $ret;
+    my $RATIO = $COUNT_ORTHOLOGS / $COUNT_GENES;
+    if ($OPT{s}) {
+        if ($OPT{v}) {
+            $ret = sprintf("%.1f = $COUNT_ORTHOLOGS / $COUNT_GENES", $RATIO * 100);
+        } else {
+            $ret = $RATIO * 100;
+        }
+    } else {
+        $ret = paste_blocks(@OUT);
+        $ret .= "\n";
+        $ret .= sprintf("$COUNT_ORTHOLOGS/$COUNT_GENES = %.1f", $RATIO * 100);
+    }
+    return $ret;
+}
 
 sub read_orthology {
     my ($file) = @_;
@@ -200,12 +225,12 @@ sub read_orthology {
 }
 
 sub remember_symbol_max_len {
-    my ($anchor, $gene) = @_;
+    my ($r_symbol_max_len, $gene, $anchor) = @_;
 
     my $symbol = $SYMBOL{$gene} || 0;
-    if (! defined($SYMBOL_MAX_LEN{$anchor}) ||
-        length($symbol) > $SYMBOL_MAX_LEN{$anchor}) {
-        $SYMBOL_MAX_LEN{$anchor} = length($symbol);
+    if (! defined(${$r_symbol_max_len}{$anchor}) ||
+        length($symbol) > ${$r_symbol_max_len}{$anchor}) {
+        ${$r_symbol_max_len}{$anchor} = length($symbol);
     }
 }
 
@@ -237,15 +262,15 @@ sub paste_blocks {
 }
 
 sub format_symbol {
-    my ($anchor, $geneid) = @_;
+    my ($geneid, $r_gene, $r_highlight, $symbol_max_len) = @_;
 
     if ($SYMBOL{$geneid}) {
         my $symbol = $SYMBOL{$geneid};
-        my $padding_len = $SYMBOL_MAX_LEN{$anchor} - length($symbol);
+        my $padding_len = $symbol_max_len - length($symbol);
         my $padding = " " x $padding_len;
-        if ($GENE{$geneid}) {
+        if (${$r_gene}{$geneid}) {
             return "[[ " . $symbol . $padding . "]]";
-        } elsif ($HIGHLIGHT_GENE{$geneid}) {
+        } elsif (${$r_highlight}{$geneid}) {
             return " [ " . $symbol . $padding . "]";
         } else {
             return "   " . $symbol;
