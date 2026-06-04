eef26a68109c1db8af1d2e78ccd9daff1ee31c13
Hirokazu Chiba <hchiba1@gmail.com>
2026-06-05 00:42:50 +0900

    Refactor merge_orthologous_pairs.pl: remove comments

--- bin/merge_orthologous_pairs.pl
+++ bin/merge_orthologous_pairs.pl
@@ -18,69 +18,42 @@ while (<>) {
         print STDERR "Error: $_\n";
         die;
     }
-    my $gene1 = $f[0];
+    my $genes1 = $f[0];
     my $gene2 = $f[1];
     my $type = $f[2];
-    $TYPE{$gene1}{$gene2} = $type;
+    $TYPE{$genes1}{$gene2} = $type;
 }
 
-for my $gene1 (sort keys %TYPE) {
-    my @gene2 = sort { $TYPE{$gene1}{$a} cmp $TYPE{$gene1}{$b} } keys %{$TYPE{$gene1}};
+for my $genes1 (sort keys %TYPE) {
+    my @gene2 = keys %{$TYPE{$genes1}};
     if (@gene2 == 1) {
-        my $type = $TYPE{$gene1}{$gene2[0]};
-        print join("\t",
-                   $gene1,
-                   $gene2[0],
-                   $type
-            ), "\n";
-    } else {
-        my @type = check_types($gene1, @gene2);
-        if (@type == 1) {
-            my $type = $type[0];
-            print join("\t",
-                       $gene1,
-                       join(",", @gene2),
-                       $type
-                ), "\n";
-        } else {
-            my $types = join(",", @type);
-            my $genes2 = join(",", @gene2);
-            print join("\t",
-                       $gene1,
-                       $genes2,
-                       $types
-                ), "\n";
-
-            # if ($types eq "one,several") {
-            # } else {
-            #     print "== $types\n";
-            #     for my $gene2 (@gene2) {
-            #         print join("\t",
-            #                    $gene1,
-            #                    $gene2,
-            #                    $TYPE{$gene1}{$gene2},
-            #             ), "\n";
-            #     }
-            # }
-
-        }
+        my $gene2 = $gene2[0];
+        print join("\t", $genes1, $gene2, $TYPE{$genes1}{$gene2}), "\n";
+        next;
     }
+
+    @gene2 = sort { $TYPE{$genes1}{$a} cmp $TYPE{$genes1}{$b} } @gene2;
+    my @type = uniq_type($genes1, @gene2);
+
+    if (@type == 1) {
+        my $type = $type[0];
+        print join("\t", $genes1, join(",", @gene2), $type), "\n";
+        next;
+    }
+
+    print join("\t", $genes1, join(",", @gene2), join(",", @type)), "\n";
 }
 
-sub check_types {
-    my ($gene1, @gene2) = @_;
+################################################################################
+### Function ###################################################################
+################################################################################
 
-    # my $type = $TYPE{$gene1}{$gene2[0]};
-    # for (my $i=1; $i<@gene2; $i++) {
-    #     if ($TYPE{$gene1}{$gene2[$i]} ne $type) {
-    #         return 0;
-    #     }
-    # }
-    # return $type;
+sub uniq_type {
+    my ($genes1, @gene2) = @_;
 
     my %hash;
     for my $gene2 (@gene2) {
-        my $type = $TYPE{$gene1}{$gene2};
+        my $type = $TYPE{$genes1}{$gene2};
         $hash{$type} = 1;
     }
     my @type = sort { $a cmp $b } keys %hash;
