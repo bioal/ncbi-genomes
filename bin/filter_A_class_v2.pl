4583d163caf8b759ebac11791e911a4e5b29e672
Hirokazu Chiba <hchiba1@gmail.com>
2026-06-06 09:19:45 +0900

    Simplify filter_A_class_v2.pl output: reduce columns

--- bin/filter_A_class_v2.pl
+++ bin/filter_A_class_v2.pl
@@ -14,10 +14,23 @@ while (<>) {
     chomp;
     my @f = split(/\t/, $_, -1);
     my $orthology = $f[6];
-    my $confirmed = $f[10];
+    my $confirmed = $f[-1];
     if ($orthology > 0.5) {
         if ($confirmed =~ /[NT]/) {
-            print "$_\n";
+            my @f = split("\t", $_, -1);
+            if ($f[4] ne $f[7]) {
+                print STDERR "Error: $_\n";
+                die;
+            }
+            if ($f[5] ne $f[8]) {
+                print STDERR "Error: $_\n";
+                die;
+            }
+            if ($f[6] ne $f[9]) {
+                print STDERR "Error: $_\n";
+                die;
+            }
+            print join("\t", @f[0..6,10..$#f]), "\n";
         }
     }
 }
